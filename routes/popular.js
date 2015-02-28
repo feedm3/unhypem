fb5c2983f4b2699d2b7b745e159fb0028796b279
Fabian Dietenberger <piano.fabian@t-online.de>
2015-02-28 16:03:01 +0100

    Get only the songs as one single object. The timestamp moves to the response header

--- routes/popular.js
+++ routes/popular.js
@@ -2,11 +2,27 @@ var express = require('express');
 var dbAdapter = require('./../dbAdapter');
 var router = express.Router();
 
-/* GET users listing. */
+/**
+ * GET the latest popular songs.
+ * The timestamp of the popular songs gets written into
+ * the header field 'timestamp'.
+ */
 router.get('/', function (req, res) {
     dbAdapter.getPopularSongs(function (latestEntry) {
+        var objectId = latestEntry._id;
+        res.header('timestamp', dateFromObjectId(objectId));
         res.json(latestEntry.songs);
     });
 });
+
+/**
+ * Convert an object ID to a date.
+ *
+ * @param objectId the object id to convert
+ * @returns {Date} the date
+ */
+function dateFromObjectId(objectId) {
+    return new Date(parseInt(objectId.toString().substring(0, 8), 16) * 1000);
+}
 
 module.exports = router;
