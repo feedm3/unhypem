c8c750b203a77ffe1c80be8fed1b8cf2af5e7ec4
Fabian Dietenberger <piano.fabian@t-online.de>
2015-07-09 19:17:02 +0200

    Add songs schema

---
+++ app/model/songs.js
@@ -0,0 +1,31 @@
+var mongoose = require('mongoose');
+
+var SongSchema = mongoose.Schema({
+    artist: {
+        type: String,
+        required: true,
+        index: true
+    },
+    title: {
+        type: String,
+        required: true,
+        index: true
+    },
+    hypemMediaId: {
+        type: String,
+        required: true,
+        index: true
+    },
+    hypemLoveCount: number,
+    mp3Url: String,
+    streamUrl: String,
+    soundcloudUrl: String,
+    soundcloudId: String,
+    waveformUrl: String
+});
+
+var Songs = mongoose.model('songs', SongSchema);
+
+module.exports = {
+    Songs: Songs
+};
