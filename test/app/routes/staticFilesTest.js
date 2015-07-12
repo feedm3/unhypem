e1a740feb5dcc888171777c5cc230b3355a982a0
Fabian Dietenberger <piano.fabian@t-online.de>
2015-07-12 10:38:32 +0200

    Move tests to the correct package

--- test/app/routes/staticFilesTest.js
+++ test/app/routes/staticFilesTest.js
@@ -1,5 +1,5 @@
 var request = require('supertest');
-var app = require('../app.js');
+var app = require('../../../app.js');
 
 describe('Request the root path', function (done) {
     it('should return 200 status code', function (done) {
