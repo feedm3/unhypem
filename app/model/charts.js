e0b63ddeb54617099392c19792d93c041502d32e
Fabian Dietenberger <piano.fabian@t-online.de>
2015-08-16 17:11:11 +0200

    Add charts schema

---
+++ app/model/charts.js
@@ -0,0 +1,39 @@
+/**
+ * @author Fabian Dietenberger
+ */
+
+'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var ChartSchema = mongoose.Schema({
+    type: {
+        type: String,
+        required: true,
+        index: true,
+        default: 'popular'
+    },
+    timestamp: {
+        type: Date,
+        required: true
+    },
+    songs: [{
+        position: Number,
+        song: {
+            type: [Schema.Types.ObjectId],
+            ref: 'songs'
+        }
+    }]
+});
+
+var Charts = mongoose.model('charts', ChartSchema);
+
+Charts.schema.path('type').validate(function (value) {
+    return /popular|remix-only|no-remix/.test(value);
+}, 'Invalid input. Type must be \'popular\', \'remix-only\' or \'no-remix\'');
+
+module.exports = {
+    Charts: Charts
+};
+ 
