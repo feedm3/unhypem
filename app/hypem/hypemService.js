fae166bdd21fd9605620327345002fefe2a7394c
Fabian Dietenberger <piano.fabian@t-online.de>
2015-08-16 18:05:20 +0200

    Implement new hypem service with mongoose

--- app/hypem/hypemService.js
+++ app/hypem/hypemService.js
@@ -9,31 +9,86 @@
 var hypemCrawler = require('./hypemCrawler'),
     CronJob = require('cron').CronJob,
     Songs = require('../model/songs').Songs,
-    dbAdapter = require('../database/dbAdapter');
+    Charts = require('../model/charts').Charts,
+    async = require('async'),
+    _ = require('lodash');
 
 var job;
 
 exports.start = function () {
-
     // seconds minutes hours dayOfMonth months dayOfWeek
     job = new CronJob('0 */5 * * * *', function () {
-        startCrawler();
+        console.log("Start updating charts");
+        crawlAndSavePopularSongs();
     });
     job.start();
+};
+
+exports.startNow = function () {
+    crawlAndSavePopularSongs();
 };
 
 exports.stop = function () {
     job.stop();
 };
 
-function startCrawler() {
-    var d = new Date();
-    var currentDate = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
-    console.log("Start updating songs...");
-    hypemCrawler.updatePopularSongs(callbackSongsUpdated);
-}
+function crawlAndSavePopularSongs() {
+    hypemCrawler.resolvePopularList(function (err, songs) {
+        var popularSongs = [];
+        async.each(songs, function (songRaw, done) {
 
-function callbackSongsUpdated(popularSongsDTO) {
-    console.log("Updating finished.");
-    dbAdapter.savePopularSongs(popularSongsDTO);
+            var position = _.findKey(songs, songRaw);
+
+            Songs.findOne({hypemMediaId: songRaw.mediaid}, function (err, song) {
+                if (err) {
+                    console.error("Error finding song in database. " + err);
+                    throw err;
+                }
+                if (!song) {
+                    // song does not exist in database so we save it
+                    var songModel = new Songs({
+                        artist: songRaw.artist,
+                        title: songRaw.title,
+                        hypemMediaId: songRaw.mediaid,
+                        hypemLoveCount: songRaw.love_count,
+                        streamUrl: songRaw.streamUrl,
+                        soundcloudUrl: songRaw.soundcloudUrl
+                    });
+                    songModel.save(function (err, song) {
+                        if (err) {
+                            console.error("Could not save song to database. " + err);
+                            throw err;
+                        }
+                        popularSongs.push({
+                            position: position,
+                            song: song._id
+                        });
+                        done();
+                    });
+                } else {
+                    popularSongs.push({
+                        position: position,
+                        song: song._id
+                    });
+                    done();
+                }
+            });
+        }, function (err) {
+            if (err) {
+                console.error("Error saving popular songs. " + err);
+                throw err;
+            }
+            var charts = new Charts({
+                timestamp: new Date(),
+                songs: popularSongs
+            });
+            charts.save(function (err, chartsSaved) {
+                if (err) {
+                    console.error("Error saving charts. " + err);
+                    throw err;
+                }
+                console.log("Charts saved");
+            });
+        });
+    });
 }
