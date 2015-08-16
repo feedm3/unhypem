659f38de6c5558f239b295a543bbaabfaf1bd0f7
Fabian Dietenberger <piano.fabian@t-online.de>
2015-08-16 19:23:03 +0200

    Adapt songs test

--- test/app/routes/songTets.js
+++ test/app/routes/songTets.js
@@ -8,9 +8,23 @@ var request = require('supertest'),
     app = require('../../../app.js');
 
 describe('Request the song path', function () {
-    it('should return 200 status code', function (done) {
-        request(app)
-            .get('/song/dwdawd')
-            .expect(200, done);
+    describe('if the media id is correct', function () {
+
+    });
+
+    describe('if the media id does not exist', function () {
+        it('should return 404', function (done) {
+            request(app)
+                .get('/songs/ddwadwdawd')
+                .expect(404, done);
+        });
+    });
+
+    describe('if no media id is given', function () {
+        it('should return 404', function (done) {
+            request(app)
+                .get('/songs')
+                .expect(404, done);
+        });
     });
 });
