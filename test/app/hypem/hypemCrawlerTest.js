05cc113f129d647cb954f4fb4464077ce7ae2893
Fabian Dietenberger <piano.fabian@t-online.de>
2015-09-17 19:26:47 +0200

    Remove soundcloud check from hypem crawler test

--- test/app/hypem/hypemCrawlerTest.js
+++ test/app/hypem/hypemCrawlerTest.js
@@ -26,6 +26,7 @@ describe('Resolve all songs on the popular list', function () {
     it('every object should contain song informations', function (done) {
         hypemCrawler.getAllPopularSongs(function (err, songs) {
             _.forIn(songs, function (song, num) {
+                /* note that at this point a song does have all fields from hypem */
                 num.should.be.above(0);
                 num.should.be.below(51);
 
@@ -33,13 +34,6 @@ describe('Resolve all songs on the popular list', function () {
                 song.title.should.be.a('string');
                 song.loved_count.should.be.a('number');
                 song.mediaid.should.be.a('string');
-
-                song.should.include.all.keys(['artist', 'title', 'hypemMediaId', 'hypemLovedCount']);
-
-                if (util.isSoundcloudUrl(song.streamUrl)) {
-                    song.soundcloudUrl.should.be.a('string');
-                    song.sould.include.all.keys(['soundcloudUrl', 'soundcloudId', 'waveformUrl']);
-                }
             });
             done();
         });
