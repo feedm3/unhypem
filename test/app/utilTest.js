93a269f2f5f7b7117d1c27c39b0959e0d55109c2
Fabian Dietenberger <piano.fabian@t-online.de>
2015-09-21 20:00:29 +0200

    Add test for the util methods

---
+++ test/app/utilTest.js
@@ -0,0 +1,27 @@
+/**
+ * @author Fabian Dietenberger
+ */
+
+'use strict';
+
+require('chai').should();
+
+var util = require('../../app/util');
+
+describe('Check if url is a soundcloud url', function () {
+    it('should return true if url is valid', function () {
+        util.isSoundcloudUrl("https://soundcloud.com/porter-robinson/porter-robinson-divinity-odesza-remix").should.be.true;
+        util.isSoundcloudUrl("http://soundcloud.com/porter-robinson/porter-robinson-divinity-odesza-remix").should.be.true;
+    });
+    it('should return false if url is not valid', function () {
+        util.isSoundcloudUrl("fsefesf").should.be.false;
+        util.isSoundcloudUrl().should.be.false;
+        util.isSoundcloudUrl(undefined).should.be.false;
+        util.isSoundcloudUrl(null).should.be.false;
+        util.isSoundcloudUrl("https://soundcloud.com/not/found").should.be.false;
+        util.isSoundcloudUrl("http://soundcloud.com/not/found").should.be.false;
+        util.isSoundcloudUrl(123432).should.be.false;
+    });
+});
+ 
+ 
