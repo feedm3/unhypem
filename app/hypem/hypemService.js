5310b967800357ce505b242c631801e1090f441d
Fabian Dietenberger <piano.fabian@t-online.de>
2015-12-01 17:43:33 +0100

    Remove old code and finish crawling correctly

--- app/hypem/hypemService.js
+++ app/hypem/hypemService.js
@@ -78,96 +78,39 @@ function crawlAndSavePopularSongs(done) {
                 logger.error("Error finding song in database. So saving will also be cancelled. " + err);
                 throw err;
             });
-            //
-            //Songs.findOne({hypemMediaId: songRaw.mediaid}, function (err, song) {
-            //    if (err) {
-            //        logger.error("Error finding song in database. " + err);
-            //        throw err;
-            //    }
-            //    if (song) {
-            //        song.hypemLovedCount.push({
-            //            timestamp: moment(),
-            //            count: songRaw.loved_count
-            //        });
-            //        song.save(function (err, song) {
-            //            if (err) {
-            //                logger.error("Could not update song in database. " + err);
-            //                logger.error(song);
-            //                throw err;
-            //            }
-            //            popularSongs.push({
-            //                position: position,
-            //                song: song._id
-            //            });
-            //            done();
-            //        });
-            //    } else {
-            //        // song does not exist in database so we save it
-            //        var songModel = new Songs({
-            //            artist: songRaw.artist,
-            //            title: songRaw.title,
-            //            hypemMediaId: songRaw.mediaid,
-            //            hypemLovedCount: {
-            //                timestamp: moment(),
-            //                count: songRaw.loved_count
-            //            },
-            //            streamUrl: songRaw.streamUrl,
-            //            soundcloudUrl: songRaw.soundcloudUrl,
-            //            soundcloudId: songRaw.soundcloudId,
-            //            waveformUrl: songRaw.waveformUrl
-            //        });
-            //        songModel.save(function (err, song) {
-            //            if (err) {
-            //                logger.error("Could not save song to database. " + err);
-            //                logger.error(song);
-            //                throw err;
-            //            }
-            //            popularSongs.push({
-            //                position: position,
-            //                song: song._id
-            //            });
-            //            done();
-            //        });
-            //    }
-            //});
         }, function (err) {
             if (err) {
                 logger.error("Error saving popular songs. " + err);
                 throw err;
             }
-            new ChartsModel({
-                timestamp: moment(),
-                type: 'popular'
-            }).save()
+            ChartsModel
+                .forge()
+                .where({
+                    timestamp: moment(),
+                    type: 'popular'
+                })
+                .save()
                 .then(function (chartEntry) {
                     var chartId = chartEntry.get('id');
-                    async.each(popularSongs, function (entry, finished) {
+                    async.each(popularSongs, function (entry, done) {
                         new ChartSongsModel({
                             chart_id: chartId,
                             song_id: entry.songId,
                             position: entry.position
-                        }).save().return();
+                        }).save().then(function () {
+                            done();
+                        });
                     }, function (err) {
-                        throw err;
+                        if (err) {
+                            throw err;
+                        }
+                        done();
                     });
-                }).catch(function (err) {
-                logger.error("Could not create chart. " + err);
-                throw err;
-            });
-            //var charts = new ChartsModel({
-            //    timestamp: moment(),
-            //    songs: popularSongs
-            //});
-            //charts.save(function (err) {
-            //    if (done) {
-            //        done(err);
-            //    }
-            //    if (err) {
-            //        logger.error("Error saving charts. " + err);
-            //        throw err;
-            //    }
-            //    logger.info("ChartsModel saved");
-            //});
+                })
+                .catch(function (err) {
+                    logger.error("Could not create chart. " + err);
+                    throw err;
+                });
         });
     });
 }
