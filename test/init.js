7116943c470cee177cea9a43b6af3e4b535e428b
Fabian Dietenberger <piano.fabian@t-online.de>
2015-12-01 17:53:52 +0100

    Init test correctly

--- test/init.js
+++ test/init.js
@@ -4,15 +4,13 @@
 
 'use strict';
 
-var app = require('../app'),
-    db = require('../app/config/db'),
+var db = require('../app/config/db'),
     migration =require('../app/config/migration'),
     hypemService = require('../app/hypem/hypemService');
 
 before(function (done) {
     this.timeout(10000);
 
-    app.set('port', process.env.PORT || 3000);
     migration.up(db.knex).then(function () {
         hypemService.startNow(function (err) {
                     if (err) {
