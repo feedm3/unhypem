2dcb17c1810dcd6fb6f97efc195b7cc9d9d8d719
Fabian Dietenberger <piano.fabian@t-online.de>
2015-12-01 17:43:56 +0100

    Init test with the new db

--- test/init.js
+++ test/init.js
@@ -5,25 +5,23 @@
 'use strict';
 
 var app = require('../app'),
-    mongoose = require('mongoose'),
+    db = require('../app/config/db'),
+    migration =require('../app/config/migration'),
     hypemService = require('../app/hypem/hypemService');
 
 before(function (done) {
     this.timeout(10000);
 
     app.set('port', process.env.PORT || 3000);
-
-    mongoose.createConnection('mongodb://' + process.env.MONGOLAB_URI, function (err) {
-        if (err) {
-            throw err;
-        }
+    migration.up(db.knex).then(function () {
         hypemService.startNow(function (err) {
-            if (err) {
-                throw err;
-            }
+                    if (err) {
+                        throw err;
+                    }
             console.log("Setup done");
             done();
         });
+    }).catch(function (err) {
+        throw err;
     });
-
 });
