2272cb29fc858fe3c9e59f5142b7bdc8f27349bb
Fabian Dietenberger <piano.fabian@t-online.de>
2016-01-09 16:44:04 +0100

    Fix wrong main file in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const path = require('path');
 // const webpack = require('webpack');
 
 module.exports = {
-    entry: './public/app/song-table.js',
+    entry: './public/app/main.js',
     output: {
         path: path.resolve(__dirname, './public'),
         filename: 'bundle.js',
