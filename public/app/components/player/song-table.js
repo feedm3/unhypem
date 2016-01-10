783da2c55e3fbe16d4abd3db12b51b53601c2b76
Fabian Dietenberger <fabian.dietenberger@ametras.com>
2016-01-10 16:55:23 +0100

    Load all songs from localhost server and display them

--- public/app/components/player/song-table.js
+++ public/app/components/player/song-table.js
@@ -4,55 +4,28 @@
 
 import React from 'react';
 import SongTableRow from './song-table-row';
+import request from 'superagent';
 
 class SongTable extends React.Component {
     constructor() {
         super();
         this.state = {
-            songs: [
-                {
-                    'id': 1057,
-                    'artist': 'Kiiara',
-                    'title': 'Feels (Felix Cartal Remix)',
-                    'hypemMediaId': '2esxt',
-                    'hypemLovedCount': 3429,
-                    'streamUrl': 'https://api.soundcloud.com/tracks/240678348/stream?client_id=ae22eeed80859593bd5159ce968b1d38',
-                    'soundcloudUrl': 'http://soundcloud.com/felixcartal/kiiara-feels-felix-cartal-remix',
-                    'soundcloudId': '240678348',
-                    'waveformUrl': 'https://w1.sndcdn.com/qD5jb5DXBuD3_m.png',
-                    'position': 1
-
-                },
-                {
-                    'id': 1078,
-                    'artist': 'Sia',
-                    'title': 'Reaper (feat. Kanye West)',
-                    'hypemMediaId': '2etea',
-                    'hypemLovedCount': 1974,
-                    'streamUrl': 'http://poponandon.com/wp-content/uploads/2016/01/07-Reaper-Ft.-Kanye-West.mp3',
-                    'soundcloudUrl': null,
-                    'soundcloudId': null,
-                    'waveformUrl': null,
-                    'position': 2
-                },
-                {
-                    'id': 85,
-                    'artist': 'Mura Masa',
-                    'title': 'Love For That (feat. Shura)',
-                    'hypemMediaId': '2dpga',
-                    'hypemLovedCount': 4611,
-                    'streamUrl': null,
-                    'soundcloudUrl': 'http://soundcloud.com/muramasamusic/love-for-that',
-                    'soundcloudId': null,
-                    'waveformUrl': null,
-                    'position': 19
-                }
-            ]
+            songs: []
         };
     }
 
+    componentDidMount() {
+        request.get("http://localhost:3000/popular")
+            .end((err, response)  => {
+                const popular = JSON.parse(response.text);
+                this.setState({
+                    'songs': popular.songs
+                });
+            });
+    }
+
     render() {
-        const songTableRows = this.state.songs.map(function(song, index) {
+        const songTableRows = this.state.songs.map(function (song, index) {
             return <SongTableRow song={song} key={index}/>;
         });
         return (
