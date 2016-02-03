d7a2a7760d2c2706456ee2333b81355bfd653da0
Fabian Dietenberger <piano.fabian@t-online.de>
2016-02-03 13:20:37 +0100

    Bundle all styles and images into one styles.css file

--- public/app/main.js
+++ public/app/main.js
@@ -5,6 +5,10 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './components/app';
+import '../assets/css/style.css';
+import '../assets/css/bootstrap.min.css';
+import '../assets/css/buttons.css';
+import '../assets/css/player.css';
 
 // preload the player with all songs and start loading the first song (that the player calculates the duration)
 
