3802b9f9183c2f3acffad2ce495c30a85528de48
Fabian Dietenberger <piano.fabian@t-online.de>
2016-03-01 08:30:06 +0100

    Exclude bootstrap.min.js from the bundled style

--- public/app/main.js
+++ public/app/main.js
@@ -6,7 +6,6 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import App from './components/app';
 import '../assets/css/style.css';
-import '../assets/css/bootstrap.min.css';
 import '../assets/css/buttons.css';
 import '../assets/css/player.css';
 
