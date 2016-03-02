be26d187edd539a05d9874b3145799b3b18d6ef5
Fabian Dietenberger <piano.fabian@t-online.de>
2016-03-02 07:13:01 +0100

    Add states

---
+++ public/app/constants/action.js
@@ -0,0 +1,16 @@
+/**
+ * This file defines the actions which the dispatcher can handle.
+ *
+ * @author Fabian Dietenberger
+ */
+
+'use strict';
+ 
+export default {
+    SELECT: 'SELECT_SONG',
+    PLAY: 'PLAY_SONG',
+    PAUSE: 'PAUSE_SONG',
+    FORWARD: 'NEXT_SONG',
+    REWIND: 'PREVIOUS_SONG',
+    CHANGE_VOLUME: 'CHANGE_VOLUME'
+};
