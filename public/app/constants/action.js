196bc8d9bcf52fc955c8a8158b1e7849e10f93e5
Fabian Dietenberger <piano.fabian@t-online.de>
2016-03-02 07:39:55 +0100

    Add additional states and put some comments on them

--- public/app/constants/action.js
+++ public/app/constants/action.js
@@ -7,10 +7,39 @@
 'use strict';
  
 export default {
-    SELECT: 'SELECT_SONG',
+
+    /**
+     * Select the current song. Put the song into the state.
+     */
+    SELECT_SONG: 'SELECT_SONG',
+
+    /**
+     * Select the position of the current song. Put the position (in millis) into the state.
+     */
+    SELECT_POSITION: 'SELECT_POSITION',
+
+    /**
+     * Play the current song. Put the position (in millis) into the state.
+     */
     PLAY: 'PLAY_SONG',
+
+    /**
+     * Pause the current song. No state.
+     */
     PAUSE: 'PAUSE_SONG',
+
+    /**
+     * Play the next song. No state.
+     */
     FORWARD: 'NEXT_SONG',
+
+    /**
+     * Play the song from the beginning or select the previous song. No state.
+     */
     REWIND: 'PREVIOUS_SONG',
+
+    /**
+     * Change the volume of the player. Put the volume (in percent) into the state.
+     */
     CHANGE_VOLUME: 'CHANGE_VOLUME'
 };
