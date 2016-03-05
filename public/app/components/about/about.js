d41d7a9e8153d7f90d7c530545544bd46c8e4de3
Fabian Dietenberger <piano.fabian@t-online.de>
2016-03-05 11:20:13 +0100

    Add some text to the about page

--- public/app/components/about/about.js
+++ public/app/components/about/about.js
@@ -9,7 +9,11 @@ import React from 'react';
 export default () => {
     return (
         <div>
-            <p>The code of this webapp can be found on <a href="https://github.com/feedm3/unhypem" target="_blank">Github</a></p>
+            <p>Unhypem is a site to explore the current popular charts from <a href="http://hypem.com/popular">hypem</a>.
+                The page is free to use. Have fun listening to good music and feel free to share this page with your
+                friends.</p>
+            <p>The code of this webapp can be found on <a href="https://github.com/feedm3/unhypem" target="_blank">Github</a>
+            </p>
         </div>
     );
 };
