93a27babf240e7378e413482ee1414c89a2cf394
Fabian Dietenberger <fabian.dietenberger@sinnerschrader.com>
2016-05-26 19:07:01 +0200

    Implement share button with tether component

--- public/app/components/player/share-button.js
+++ public/app/components/player/share-button.js
@@ -5,55 +5,58 @@
 'use strict';
 
 import React from 'react';
+import TetherComponent from 'react-tether';
 import SvgIcon from '../common/svg-icon';
 
 export default class ShareButton extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            visible: false
+            popupVisible: false
         };
     }
 
-    // TODO refactor this class and the popup concept (styling)
-
-    togglePopup() {
-        const sharePopup = document.getElementById('sharePopup');
-        if (!sharePopup.style.display || sharePopup.style.display === 'none') {
-            sharePopup.style.display = 'block';
-        } else {
-            sharePopup.style.display = 'none';
-        }
-    }
-
     render() {
-        const soundcloudUrlStyle = this.props.soundcloudUrl ? {display: 'block', paddingBottom: '5px'} : {display: 'none'};
+        const soundcloudUrlStyle = this.props.soundcloudUrl ? {
+            display: 'block',
+            paddingBottom: '5px'
+        } : {display: 'none'};
         return (
             <div>
-                <SvgIcon
-                    id='ic_share_black_24px'
-                    title='Share'
-                    width='24px'
-                    height='24px'
-                    onClick={() => this.togglePopup() }
-                />
-                <div className='popup-wrapper'>
-                    <div className='share-popup'
-                         id='sharePopup'
-                         onMouseLeave={() => this.togglePopup()}>
-                        <a href={this.props.soundcloudUrl}
-                           target='_blank'
-                           style={soundcloudUrlStyle}
-                           className='share-popup-link'>
-                            Open on soundcloud
-                        </a>
-                        <a href={'http://hypem.com/track/' + this.props.hypemMediaId}
-                           target='_blank'
-                           className='share-popup-link'>
-                            Open on hypem
-                        </a>
-                    </div>
-                </div>
+                <TetherComponent
+                    attachment='bottom center'
+                    offset='15px 0'
+                    constraint={[{
+                        to: 'window',
+                        attachment: 'both'
+                    }]}>
+                    <SvgIcon
+                        id='ic_share_black_24px'
+                        title='Share'
+                        width='24px'
+                        height='24px'
+                        onClick={() => this.setState({popupVisible: !this.state.popupVisible}) }
+                    />
+                    {
+                        this.state.popupVisible &&
+                        <div>
+                            <div className='share-button-popup'>
+                                <a href={this.props.soundcloudUrl}
+                                   target='_blank'
+                                   style={soundcloudUrlStyle}
+                                   className='share-popup-link'>
+                                    Open on soundcloud
+                                </a>
+                                <a href={'http://hypem.com/track/' + this.props.hypemMediaId}
+                                   target='_blank'
+                                   className='share-popup-link'>
+                                    Open on hypem
+                                </a>
+                            </div>
+                            <div style={{marginBottom: '-5px'}} className="tether-tooltip-triangle"></div>
+                        </div>
+                    }
+                </TetherComponent>
             </div>
         );
     }
